658fe2ba9d40d40a1dbebc366e89d2b7eb0873f4
E. K. Yi <wolf@teoal.net>
2026-03-08 23:53:41 +0900

    b2-ch03 한 문장으로 단순 skip-gram 실습

--- b2-rnn/ch03/train.py
+++ b2-rnn/ch03/train.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from common.trainer import Trainer
 from common.optimizer import Adam
 from simple_cbow import SimpleCBOW
+from simple_skip_gram import SimpleSkipGram
 from common.util import preprocess, create_contexts_target, convert_one_hot
 
 window_size = 1
@@ -20,6 +21,7 @@ target = convert_one_hot(target, vocab_size)
 contexts = convert_one_hot(contexts, vocab_size)
 
 model = SimpleCBOW(vocab_size, hidden_size)
+# model = SimpleSkipGram(vocab_size, hidden_size)
 optimizer = Adam()
 trainer = Trainer(model, optimizer)
 trainer.fit(contexts, target, max_epoch, batch_size)
