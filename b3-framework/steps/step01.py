c97bbd4876d5f460e94db20ea1be31d79f6be14d
E. K. Yi <wolf@teoal.net>
2026-03-27 16:20:27 +0900

    b3-s01 Variable 클래스 시작

---
+++ b3-framework/steps/step01.py
@@ -0,0 +1,14 @@
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+import numpy as np
+
+data = np.array(1.0)
+x = Variable(data)
+print(x.data)
+
+data = np.array(2.0)
+x.data = data
+print(x.data)
