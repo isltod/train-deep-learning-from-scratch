dfa28a8f630b79e1c5031f9d422bdd4321a87825
E. K. Yi <wolf@teoal.net>
2026-03-30 18:03:34 +0900

    b3-s34 sin cos 함수 추가하고 고차미분 실습

---
+++ b3-framework/dezero/functions.py
@@ -0,0 +1,33 @@
+import numpy as np
+from dezero.core import Function
+
+
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy):
+        # 이걸 그냥 x로 받으면 튜플 또는 리스트 객체가 담긴다...그 안의 Variable 받으려면 튜플로 받아야된다...
+        (x,) = self.inputs
+        gx = gy * cos(x)
+        return gx
+
+
+def sin(x):
+    return Sin()(x)
+
+
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy):
+        (x,) = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+
+def cos(x):
+    return Cos()(x)
