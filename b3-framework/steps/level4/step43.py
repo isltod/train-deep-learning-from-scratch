79f5f740a44c2cce8b0784b8f613d99be494ac5c
E. K. Yi <wolf@teoal.net>
2026-04-01 21:21:41 +0900

    b3-s43 신경망 기본 구조 실습

---
+++ b3-framework/steps/level4/step43.py
@@ -0,0 +1,60 @@
+import sys
+
+sys.path.append("b3-framework")
+
+from dezero import Variable
+import dezero.functions as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+# 이번에는 선형 회귀가 아니라 sin 회귀에 잔차 더해놓고...
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+# x(100,1) * W1(1,10) -> t(100,10) * W2(10,1) -> y_pred(100,1)
+I, H, O = 1, 10, 1
+W1 = Variable(0.01 * np.random.randn(I, H))
+b1 = Variable(np.zeros(H))
+W2 = Variable(0.01 * np.random.randn(H, O))
+b2 = Variable(np.zeros(O))
+
+
+# 순전파는 선형변환 -> 활성화 -> 선형변환...이게 회귀
+def predict(x):
+    y = F.linear(x, W1, b1)
+    y = F.sigmoid(y)
+    y = F.linear(y, W2, b2)
+    return y
+
+
+lr = 0.2
+iters = 10000
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    W1.cleargrad()
+    b1.cleargrad()
+    W2.cleargrad()
+    b2.cleargrad()
+    loss.backward()
+
+    # 단순 경사하강법으로 보정
+    W1.data -= lr * W1.grad.data
+    b1.data -= lr * b1.grad.data
+    W2.data -= lr * W2.grad.data
+    b2.data -= lr * b2.grad.data
+
+    if i % 1000 == 0:
+        print(loss)
+
+plt.scatter(x, y, s=10)
+plt.xlabel("x")
+plt.ylabel("y")
+t = np.arange(0.0, 1.0, 0.01)[:, np.newaxis]
+y_pred = predict(t)
+plt.plot(t, y_pred.data, color="r")
+plt.tight_layout()
+plt.show()
