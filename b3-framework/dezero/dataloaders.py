60f623b3289bc3aa451c2cdf52798b0f346961a1
E. K. Yi <wolf@teoal.net>
2026-04-09 22:18:10 +0900

    b3-s60 대강대강 LSTM 한 번 해보기 실습

--- b3-framework/dezero/dataloaders.py
+++ b3-framework/dezero/dataloaders.py
@@ -48,3 +48,28 @@ class DataLoader:
 
     def to_gpu(self):
         self.gpu = True
+
+
+class SeqDataLoader(DataLoader):
+    def __init__(self, dataset, batch_size, gpu=False):
+        # Dataloader인데 suffle 하지 않는다...시계열은...
+        super().__init__(dataset=dataset, batch_size=batch_size, shuffle=False, gpu=gpu)
+
+    def __next__(self):
+        if self.iteration >= self.max_iter:
+            self.reset()
+            raise StopIteration
+
+        # 거의 이 부분만 다른데...이거 원래 Dataloader 버전으로 해도 되는거 아닌가?
+        jump = self.data_size // self.batch_size
+        batch_index = [
+            (i * jump + self.iteration) % self.data_size for i in range(self.batch_size)
+        ]
+        batch = [self.dataset[i] for i in batch_index]
+
+        xp = cuda.cupy if self.gpu else np
+        x = xp.array([example[0] for example in batch])
+        t = xp.array([example[1] for example in batch])
+
+        self.iteration += 1
+        return x, t
